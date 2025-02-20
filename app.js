eee20e5a54a29407f53aa7885e36dd2c536d4521
Lokesh Jasti <lokesh_jasti@yahoo.com>
2025-02-20 21:32:11 +0530

    75%

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var swiper = new Swiper(".home-slider",{
         depth:100,
         modifier:2,
         slideShadow:true,
-    },
-    loop:true,
+    }
+    
     
 });
 var swiper = new Swiper(".home-slider1",{
@@ -28,6 +28,24 @@ var swiper = new Swiper(".home-slider1",{
     loop:true,
     autoplay:{
         delay:5000,
+        disableOnInteraction:false,
+    },
+});
+var swiper = new Swiper(".home-slider2",{
+    effect: "coverflow",
+    grabCursor:true,
+    centeredSlides:true,
+    slidesPerView:"auto",
+    coverflowEffect:{
+        rotate:0,
+        stretch:0,
+        depth:100,
+        modifier:2,
+        slideShadow:true,
+    },
+    loop:true,
+    autoplay:{
+        delay:2000,
         disableOnInteraction:false,
     },
 });
